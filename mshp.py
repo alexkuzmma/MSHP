1aef2f3194ce604694924051b79768271cf5fb9f
Alex K. <alexkuzmma@yandex.ru>
2025-10-07 12:44:30 +0300

    MSHP_07.10.25

--- mshp.py
+++ mshp.py
@@ -19,10 +19,17 @@
 #
 # print(n // 10 % 10)
 # 2
+# x = int(input())
+# z = x // 1000
+# s = x // 100 % 10
+# c = x // 10 % 10
+# v = x % 10
+#
+# print(z + s + c + v, z * s * c * v)
+# 3
 x = int(input())
-z = x // 1000
 s = x // 100 % 10
 c = x // 10 % 10
 v = x % 10
 
-print(z + s + c + v, z * s * c * v)
+print(c * 100 + s * 10 + v)
