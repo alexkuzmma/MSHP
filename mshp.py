615ff7aee0b6a005a022e7796765d248768b4074
Alex K. <alexkuzmma@yandex.ru>
2025-10-07 12:38:47 +0300

    MSHP_07.10.25

--- mshp.py
+++ mshp.py
@@ -1,14 +1,20 @@
 # тут будут мои работы в МШП 6 класс
 
 # f-строки
-import math
-name = 'Вася'
-age = 15
+# import math
+# name = 'Вася'
+# age = 15
+#
+# print(f'Меня зовут {name} и мне {age} лет')
+# print(f'В следующем году мне исполняется {age + 1} лет')
+# print(f'В прошлом году мне было {age - 1} лет')
+#
+#
+# print(math.pi)
+# print(f'Число Пи равно {math.pi:.3}')
 
-print(f'Меня зовут {name} и мне {age} лет')
-print(f'В следующем году мне исполняется {age + 1} лет')
-print(f'В прошлом году мне было {age - 1} лет')
+# 07.10.25
+# 1
+n = int(input())
 
-
-print(math.pi)
-print(f'Число Пи равно {math.pi:.3}')
+print(n // 10 % 10)
